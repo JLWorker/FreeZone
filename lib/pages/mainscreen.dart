b600c16bd24230b6ebab3a9580a4015d8be7f171
RuralJavist <4357480@bk.ru>
2024-11-07 23:44:24 +1000

    Commit 1
    # Conflicts:
    #	android/.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	android/.gradle/buildOutputCleanup/outputFiles.bin
    #	android/local.properties
    #	lib/pages/mainscreen.dart
    
    # Conflicts:
    #	android/.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	android/.gradle/buildOutputCleanup/outputFiles.bin
    #	android/local.properties
    #	lib/pages/mainscreen.dart

--- lib/pages/mainscreen.dart
+++ lib/pages/mainscreen.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:free_zone/pages/ConfigsScreen.dart';
-import 'package:free_zone/widgets/config_editor.dart' as config_editor;
-import 'package:free_zone/pages/vpnscreen.dart' as vpnscreen;
+import 'package:free_zone/pages/editcreen.dart' as edit_page;
+import 'package:free_zone/pages/vpnscreen.dart' as vpn_screen;
 import 'package:free_zone/widgets/header.dart';
 import 'package:free_zone/themes/app-style.dart';
 import 'package:free_zone/widgets/footer_image.dart';
 import 'package:free_zone/pages/edit_page.dart' as edit_page;
-import 'package:free_zone/pages/add_new_client_page.dart' as add_new_client_page;
-import 'package:free_zone/widgets/ConnectionWidget.dart' as connection_widget;
 
 import '../themes/app-style.dart';
 
@@ -26,12 +23,8 @@ class _MainScreenState extends State<Mainscreen> {
   }
 
   final List<Widget> _screens = [
-    vpnscreen.VpnScreen(),
-    config_editor.ConfigEditorScreen(),
+    vpn_screen.VpnScreen(),
     edit_page.EditPage(),
-    add_new_client_page.AddNewClientPage(),
-    connection_widget.ConnectionWidget(),
-    ConfigsScreen()
   ];
 
 
@@ -39,54 +32,26 @@ class _MainScreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppStyle.MainColor,
-        title: const Header(),
-      ),
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: FooterImage('assets/icons/globe-earth.png'), label: ""),
-          BottomNavigationBarItem(
-              icon: FooterImage('assets/icons/add.png'), label: ""),
-          BottomNavigationBarItem(
-              icon: FooterImage('assets/icons/edit.png'), label: ""),
-          BottomNavigationBarItem(
-              icon: FooterImage('assets/icons/add.png'), label: ""),
-          BottomNavigationBarItem(
-              icon: FooterImage('assets/icons/globe-earth.png'), label: ""),
-          BottomNavigationBarItem(
-              icon: FooterImage('assets/icons/globe-earth.png'), label: ""),
-        ],
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: AppStyle.footer.color,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
         appBar: AppBar(
-        title: Text('My App'),
-    ),
-    // body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: FooterImage('assets/icons/globe-earth.png'),
-          label: ""
+          backgroundColor: AppStyle.MainColor,
+          title: const Header(),
         ),
-        BottomNavigationBarItem(
-          icon: FooterImage('assets/icons/add.png'),
-          label: ""
-        ),
-        BottomNavigationBarItem(
-          icon: FooterImage('assets/icons/edit.png'),
-          label: ""
-        ),
-      ],
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: AppStyle.footer.color,
-      ),
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: FooterImage('assets/icons/globe-earth.png'), label: ""),
+            BottomNavigationBarItem(
+                icon: FooterImage('assets/icons/add.png'), label: ""),
+            BottomNavigationBarItem(
+                icon: FooterImage('assets/icons/edit.png'), label: ""),
+          ],
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: AppStyle.footer.color,
+          currentIndex: _selectedIndex,  // Устанавливаем текущий индекс
+          onTap: _onItemTapped,
+        )
     );
   }
 }
