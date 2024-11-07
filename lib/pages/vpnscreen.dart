4b1b65f55387ad9e365c37a9fd4f383985eb1335
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2024-11-07 14:00:35 +0300

    feat: add wireguard-vnp support in vpn enable/disable button click

--- lib/pages/vpnscreen.dart
+++ lib/pages/vpnscreen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ikev_flutter/flutter_vpn.dart';
-import 'package:ikev_flutter/state.dart';
 
 class VpnScreen extends StatefulWidget {
   @override
@@ -30,6 +28,46 @@ class _VpnScreenState extends State<VpnScreen> {
     FlutterVpn.prepare();
     FlutterVpn.onStateChanged.listen((s) => setState(() => state = s));
     super.initState();
+  }
+
+  Future<void> _toggleConnectionWireguard() async {
+    await _toggleWireguardVpn();
+    setState(() {
+      _isConnected = !_isConnected;
+    });
+  }
+
+  Future<void> _toggleWireguardVpn() async {
+    final wireguard = WireGuardFlutter.instance;
+
+    // Инициализируем интерфейс
+    await wireguard.initialize(interfaceName: 'wg0');
+
+    const String conf = '''[Interface]
+    PrivateKey = eDIQ44DEmrXeGRJTFNpRcoqEmaQ8BTp4uNUobhcwwnI=
+    Address = 10.8.0.2/24
+    DNS = 1.1.1.1
+    
+    [Peer]
+    PublicKey = akB9R4mC96k1/L/WJhhW9DekqAIMdsz6H67U0wVpkiU=
+    PresharedKey = 6V5FL7NgTufJOwwqQmB6G8C4FKqTjbLaTa1WEeANUT4=
+    AllowedIPs = 0.0.0.0/0, ::/0
+    PersistentKeepalive = 0
+    Endpoint = 176.124.201.152:51820
+''';
+
+    const String address = '176.124.201.152:51821';
+
+    if (!_isConnected) {
+      await wireguard.startVpn(
+        serverAddress: address,
+        wgQuickConfig: conf,
+        providerBundleIdentifier: 'com.freezone.vpn',
+      );
+
+    } else {
+      await wireguard.stopVpn();
+    }
   }
 
   @override
