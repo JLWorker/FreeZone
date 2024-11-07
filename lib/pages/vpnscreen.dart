cc80f4865301a208903d3fc99becd7ec1511ad04
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2024-11-07 14:00:35 +0300

    feat: add wireguard-vnp support in vpn enable/disable button click

--- lib/pages/vpnscreen.dart
+++ lib/pages/vpnscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wireguard_flutter/wireguard_flutter.dart';
 
 class VpnScreen extends StatefulWidget {
   @override
@@ -8,10 +9,44 @@ class VpnScreen extends StatefulWidget {
 class _VpnScreenState extends State<VpnScreen> {
   bool _isConnected = false;
 
-  void _toggleConnection() {
+  Future<void> _toggleConnection() async {
+    await _toggleWireguardVpn();
     setState(() {
       _isConnected = !_isConnected;
     });
+  }
+
+  Future<void> _toggleWireguardVpn() async {
+    final wireguard = WireGuardFlutter.instance;
+
+    // Инициализируем интерфейс
+    await wireguard.initialize(interfaceName: 'wg0');
+
+    const String conf = '''[Interface]
+    PrivateKey = eDIQ44DEmrXeGRJTFNpRcoqEmaQ8BTp4uNUobhcwwnI=
+    Address = 10.8.0.2/24
+    DNS = 1.1.1.1
+    
+    [Peer]
+    PublicKey = akB9R4mC96k1/L/WJhhW9DekqAIMdsz6H67U0wVpkiU=
+    PresharedKey = 6V5FL7NgTufJOwwqQmB6G8C4FKqTjbLaTa1WEeANUT4=
+    AllowedIPs = 0.0.0.0/0, ::/0
+    PersistentKeepalive = 0
+    Endpoint = 176.124.201.152:51820
+''';
+
+    const String address = '176.124.201.152:51821';
+
+    if (!_isConnected) {
+      await wireguard.startVpn(
+        serverAddress: address,
+        wgQuickConfig: conf,
+        providerBundleIdentifier: 'com.freezone.vpn',
+      );
+
+    } else {
+      await wireguard.stopVpn();
+    }
   }
 
   @override
